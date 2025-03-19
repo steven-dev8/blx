da841e956e1fed67b78c64ee1c28ed7248a9a6ae
Stevenn <stevenndev8@gmail.com>
2025-03-19 13:24:49 -0300

    models.py to schemas.py

---
+++ src/Models/schemas.py
@@ -0,0 +1,29 @@
+from typing import Optional, List
+from pydantic import BaseModel
+
+class User(BaseModel):
+    id: Optional[str] = None
+    name: str
+    password: str
+    number: str
+    my_product = List[Product]
+    my_orders = List[Order]
+
+
+class Product(BaseModel):
+    id: Optional[str] = None
+    user: User
+    name: str
+    description: str
+    price: float
+    avaliable: bool = False
+
+
+class Order(BaseModel):
+    id: Optional[str] = None
+    user: User
+    product: Product
+    quantity: int
+    status: bool = True
+    address: str
+    obs: Optional[str] = "No observation"
