31205979e66307641d71a76496432795f30fc7e5
Stevenn <stevenndev8@gmail.com>
2025-03-21 17:43:23 -0300

    User entity added

--- src/infra/sqlalchemy/models/models.py
+++ src/infra/sqlalchemy/models/models.py
@@ -1,11 +1,27 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean, Text
+from sqlalchemy import Column, Integer, String, Float, Boolean, Text, ForeignKey
+from sqlalchemy.orm import relationship
 from src.infra.sqlalchemy.config.database import Base
+
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    number = Column(String, nullable=False)
+
+    products = relationship('Product', back_populates='user')
 
 class Product(Base):
     __tablename__ = 'product'
 
     id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('user.id', name='fk_user'))
     name = Column(String, nullable=False)
     description = Column(Text, default='No description')
     price = Column(Float, nullable=False)
     avaliable = Column(Boolean, default=False)
+
+    user = relationship('User', back_populates='products')
