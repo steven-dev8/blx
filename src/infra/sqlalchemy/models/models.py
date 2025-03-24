42449ce0f15d18bdd2859a2d2b5a54f44e07f255
Stevenn <stevenndev8@gmail.com>
2025-03-24 13:13:30 -0300

    format adjustment

--- src/infra/sqlalchemy/models/models.py
+++ src/infra/sqlalchemy/models/models.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import relationship
 from src.infra.sqlalchemy.config.database import Base
 
 
-
 class User(Base):
     __tablename__ = 'user'
 
@@ -12,16 +11,19 @@ class User(Base):
     password = Column(String, nullable=False)
     number = Column(String, nullable=False)
 
+
     products = relationship('Product', back_populates='user')
+
 
 class Product(Base):
     __tablename__ = 'product'
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
     user_id = Column(Integer, ForeignKey('user.id', name='fk_user'))
     name = Column(String, nullable=False)
     description = Column(Text, default='No description')
     price = Column(Float, nullable=False)
     avaliable = Column(Boolean, default=False)
+    
 
     user = relationship('User', back_populates='products')
