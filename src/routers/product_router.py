7101a667299a4dba405decc68943cdf6e7a7e7f4
Stevenn <stevenndev8@gmail.com>
2025-03-25 18:13:09 -0300

    add error handling

--- src/routers/product_router.py
+++ src/routers/product_router.py
@@ -1,44 +1,48 @@
-from fastapi import APIRouter, Depends, status, Response
+from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from src.infra.sqlalchemy.repository.product import ProcessProduct
 from src.schema.schemas import Product, ProductEdit
 from src.infra.sqlalchemy.config.database import get_db
-from typing import List
+from typing import List, Union
 
 
 router = APIRouter()
 
 
 @router.post('/product', status_code=status.HTTP_201_CREATED, response_model=Product)
-def add_product(product: Product, db: Session = Depends(get_db)):
-    print(product)
-    prd_db = ProcessProduct(db).create(product)
+def add_product(product: Product, session: Session = Depends(get_db)):
+    prd_db = ProcessProduct(session).create(product)
+    if not prd_db:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="The Body has an incorrect format.")
     return prd_db
 
 
-@router.get('/product', status_code=status.HTTP_200_OK, response_model=List[Product])
-def to_list_prds(db: Session = Depends(get_db)):
-    list_prd = ProcessProduct(db).to_list()
+@router.get('/product', status_code=status.HTTP_200_OK, response_model=List[Product] or ProductErro)
+def to_list_prds(session: Session = Depends(get_db)):
+    list_prd = ProcessProduct(session).to_list()
+    if not list_prd:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No products registered.")
     return list_prd
 
 
 @router.get('/product/{id_query}', status_code=status.HTTP_200_OK, response_model=Product)
-def query_product(id_query: int, db: Session = Depends(get_db)):
-    result = ProcessProduct(db).search(id_query)
-    if result:
-        return result
-    return Response(status_code=status.HTTP_404_NOT_FOUND, content='ID not found.')
-
+def query_product(id_query: int, session: Session = Depends(get_db)):
+    result = ProcessProduct(session).search(id_query)
+    if not result:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The product with ID {id_query} was not found.")
+    return result
 
 @router.put('/product/{id_prod}')
-def edit_product(id_prod: int, product: ProductEdit, db: Session = Depends(get_db)):
-    result = ProcessProduct(db).edit_product(id_prod, product)
+def edit_product(id_prod: int, product: ProductEdit, session: Session = Depends(get_db)):
+    result = ProcessProduct(session).edit_product(id_prod, product)
+    if not result:
+        raise HTTPException(status_code=400, detail="The Body has an incorrect format.")
     return result
 
 
-@router.delete('/product/{id_prd}', status_code=status.HTTP_200_OK)
-def del_product(id_prd: int, db: Session = Depends(get_db)):
-    result = ProcessProduct(db).delete_product(id_prd)
-    if result:
-        return result
-    return Response(status_code=status.HTTP_404_NOT_FOUND, content="ID product not found")
+@router.delete('/product/{id_prd}', status_code=status.HTTP_204_NO_CONTENT)
+def del_product(id_prd: int, session: Session = Depends(get_db)):
+    result = ProcessProduct(session).delete_product(id_prd)
+    if not result:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The product with ID {id_prd} was not found.")
+    return result
--- src/routers/user_router.py
+++ src/routers/user_router.py
@@ -1,54 +1,58 @@
-from fastapi import APIRouter, Depends, status, Response
+from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from src.infra.sqlalchemy.repository.user import ProcessUser
 from src.schema.schemas import User, UserProduct, UserResponse
 from src.infra.sqlalchemy.config.database import get_db
-from typing import List
+from typing import List, Union
 
 
 router = APIRouter()
 
 
 @router.post('/user', status_code=status.HTTP_201_CREATED, response_model=UserResponse)
-def add_user(user: User, db: Session = Depends(get_db)):
-    user_db = ProcessUser(db).create(user)
-    if user_db:
-        return user_db
-    return Response(status_code=status.HTTP_400_BAD_REQUEST, content="User was not added,")
+def add_user(user: User, session: Session = Depends(get_db)):
+    user_db = ProcessUser(session).create(user)
+    if not user_db:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="The Body has an incorrect format.")
+    return user_db
 
 
 @router.get('/user', status_code=status.HTTP_200_OK, response_model=List[UserResponse])
-def to_list_users(db: Session = Depends(get_db)):
-    list_user = ProcessUser(db).to_list()
-    if list_user:
-        return list_user
-    return Response(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content='No users found')
-
+def to_list_users(session: Session = Depends(get_db)):
+    list_user = ProcessUser(session).to_list()
+    if not list_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="No users registered.")
+    return list_user
 
 @router.get('/user/{id_user}', status_code=status.HTTP_200_OK, response_model=UserResponse)
-def query_user(id_user: int, db: Session = Depends(get_db)):
-    result_user = ProcessUser(db).query_user(id_user)
-    if result_user:
-        return result_user
-    return Response(status_code=status.HTTP_404_NOT_FOUND, content='ID not found.')
-
+def query_user(id_user: int, session: Session = Depends(get_db)):
+    result_user = ProcessUser(session).query_user(id_user)
+    if not result_user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"The user with ID {id_user} was not found.")
+    return result_user
 
 @router.delete('/user/{id_user}', status_code=status.HTTP_200_OK)
-def del_user(id_user: int, db: Session = Depends(get_db)):
-    result = ProcessUser(db).delete_user(id_user)
-    if result:
-        return result
-    return Response(status_code=status.HTTP_200_OK, content="ID not found")
-
+def del_user(id_user: int, session: Session = Depends(get_db)):
+    result = ProcessUser(session).delete_user(id_user)
+    if not result:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The user with ID {id_user} was not found.")
+    return result
 
 @router.get('/user/{id_user}/products')
-def search_user_product(id_user: int, db: Session = Depends(get_db)):
-    print('aqui')
-    result = ProcessUser(db).user_product(id_user)
+def search_user_product(id_user: int, session: Session = Depends(get_db)):
+    result = ProcessUser(session).user_product(id_user)
+    if not result:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The user with ID {id_user} was not found.")
     return result
 
 
 @router.put('/user/{id_user}')
-def edit_user(id_user: int, user: User, db: Session = Depends(get_db)):
-    result = ProcessUser(db).edit_user(id_user, user)
+def edit_user(id_user: int, user: User, session: Session = Depends(get_db)):
+    result = ProcessUser(session).edit_user(id_user, user)
+    if not result:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The user with ID {id_user} was not found.")
     return result
