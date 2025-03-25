a55b7c3bb46ccd089292debbca8d692a743d1314
Stevenn <stevenndev8@gmail.com>
2025-03-24 23:20:53 -0300

    add edit_product method to User

--- src/infra/sqlalchemy/repository/product.py
+++ src/infra/sqlalchemy/repository/product.py
@@ -1,12 +1,11 @@
 from sqlalchemy.orm import Session
-from src.schema.schemas import Product #ProductResponse
+from src.schema.schemas import Product, ProductEdit
 from src.infra.sqlalchemy.models import models
 
 
 class ProcessProduct:
     def __init__(self, session: Session):
         self.session = session
-    
     
     def create(self, product: Product):
         db_product = models.Product(name=product.name,
@@ -19,18 +18,15 @@ class ProcessProduct:
         self.session.commit()
         self.session.refresh(db_product)
         return db_product
-    
-    
+     
     def to_list(self):
         products = self.session.query(models.Product).all()
         return products 
 
-    
     def search(self, id_query):
         result = self.session.query(models.Product).filter(models.Product.id == id_query).first()
         return result
 
-    
     def delete_product(self, id_prd: int):
         query = self.session.query(models.Product).filter(models.Product.id == id_prd).first()
         if query:
@@ -38,3 +34,17 @@ class ProcessProduct:
             self.session.commit()
             return f'The product {query.name} has been deleted'
         return False
+    
+    def edit_product(self, id_user: int, product: ProductEdit):
+        query = self.session.query(models.Product).filter(models.Product.id == id_user).first()
+        if query:
+            query.name = product.name
+            query.description = product.description
+            query.price = product.price
+            query.avaliable = product.avaliable
+            self.session.commit()
+            self.session.refresh(query)
+            return ProductEdit(name=query.name,
+                               description=query.description,
+                               price=query.price,
+                               avaliable=query.avaliable)
