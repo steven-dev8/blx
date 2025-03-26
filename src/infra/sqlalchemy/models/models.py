33b71f61b75d5de185c06bf38c8738899e8b1a08
Stevenn <stevenndev8@gmail.com>
2025-03-26 15:39:38 -0300

    add Contrant login, define length max for attributes and login is unique

--- src/infra/sqlalchemy/models/models.py
+++ src/infra/sqlalchemy/models/models.py
@@ -1,18 +1,24 @@
 from sqlalchemy import Column, Integer, String, Float, Boolean, Text, ForeignKey, CheckConstraint
 from sqlalchemy.orm import relationship
 from src.infra.sqlalchemy.config.database import Base
+from typing import List
 
 
 class User(Base):
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    name = Column(String, nullable=False)
-    password = Column(String, nullable=False)
-    number = Column(String, nullable=False)
+    login = Column(String(50), nullable=False, unique=True)
+    name = Column(String(70), nullable=False)
+    password = Column(String(30), nullable=False)
+    number = Column(String(11), nullable=False)
 
-    products = relationship('Product', back_populates='user')
+    products = relationship('Product', back_populates='user', cascade="all, delete-orphan")
     order = relationship('Order', back_populates='parent_user')
+
+    __table_args__ = (
+        CheckConstraint('LENGTH(login) >= 5', name='chk_login'),
+    )
 
 
 class Product(Base):
@@ -37,7 +43,7 @@ class Order(Base):
     client_id = Column(Integer, ForeignKey('user.id', name='fk_client_order'), nullable=False)
     product_id = Column(Integer, ForeignKey('product.id', name='fk_product_order'), nullable=False)
     quantity = Column(Integer, CheckConstraint('quantity > 0'), nullable=False)
-    status: Column(Boolean, default=False)
+    status = Column(Boolean, default=False)
     address = Column(String, nullable=False)
     observation = Column(String, default='No observation.')
 
