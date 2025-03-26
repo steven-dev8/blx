4d7c0d8b8ada7d2c2e87b4c1c41849fa39fa178d
Stevenn <stevenndev8@gmail.com>
2025-03-26 19:01:27 -0300

    add error handling

--- src/infra/sqlalchemy/repository/user_repository.py
+++ src/infra/sqlalchemy/repository/user_repository.py
@@ -16,6 +16,7 @@ class ProcessUser:
                 status_code=status.HTTP_400_BAD_REQUEST, 
                 detail="The provided user data is invalid. Please check the input fields."
                 )
+
         try:
             add_user = models.User(
                                 name=user.name,
@@ -26,6 +27,7 @@ class ProcessUser:
             self.session.add(add_user)
             self.session.commit()
             self.session.refresh(add_user)
+
         except IntegrityError:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST, 
@@ -33,49 +35,83 @@ class ProcessUser:
                 )
 
         return UserResponse(id=add_user.id, name=add_user.name, number=add_user.number)
-        
 
     def to_list(self):
         user_list = self.session.query(models.User.id, models.User.name, models.User.number).all()
-        if user_list:
-            format_user = [UserResponse(id=id_u, name=name_u, number=number_u) for id_u, name_u, number_u in user_list]
-            return format_user
-        return user_list
+        if not user_list:
+            raise HTTPException(
+                                status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+                                detail="Temporarily unavailable. Fix in progress."
+                                )
+
+        format_user = [UserResponse(id=id_u, name=name_u, number=number_u) for id_u, name_u, number_u in user_list]
+        return format_user
 
     def query_user(self, id_user: int):
-        result_user = self.session.query(models.User.id, models.User.name, models.User.number).filter(
-            models.User.id == id_user
-        ).first()
-        if result_user:
-            user = UserResponse(id=result_user[0], name=result_user[1], number=result_user[2])
-            return user
-        return result_user
-    
+        result_user = (
+            self.session.query(
+                models.User.id,
+                models.User.name,
+                models.User.number
+            )
+            .filter(models.User.id == id_user)
+            .first()
+        )
+
+        if not result_user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"The user with ID {id_user} doesn't exist."
+            )
+
+        return UserResponse(id=result_user[0], name=result_user[1], number=result_user[2])
+
     def delete_user(self, id_user: int):
         query = self.session.query(models.User).filter(models.User.id == id_user).first()
-        if query:
-            self.session.delete(query)
-            self.session.commit()
-            return {"message": f"The user {query.id} has been deleted"}
-        return query
+        
+        if not query:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"The user with ID {id_user} was not found."
+                )
+
+        self.session.delete(query)
+        self.session.commit()
+    
+        return None
 
     def user_product(self, id_user: int):
-        prd_query = self.session.query(models.Product).filter(models.Product.user_id == id_user).all()
+        products_query = self.session.query(models.Product).filter(models.Product.user_id == id_user).all()
         user_query = self.session.query(models.User.id, models.User.name).filter(models.User.id == id_user).first()
-        print(prd_query, user_query)
-        if user_query:
-            formate = UserProduct(id=user_query[0], name=user_query[1], products=prd_query)
-            return formate
-        return None
+
+        if not user_query:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"The user with ID {id_user} was not found."
+                )
+
+        formatted_response = UserProduct(id=user_query[0], name=user_query[1], products=prd_query)
+        return formatted_response
 
     def edit_user(self, id_user: int, user: UserEdit):
         user_query = self.session.query(models.User).filter(models.User.id == id_user).first()
-        if user_query:
-            user_query.name = user.name
-            user_query.number = user.number
-            user_query.password = user.password
-            self.session.commit()
-            self.session.refresh(user_query)
-            return UserResponse(id=user_query.id, name=user_query.name, number=user_query.number)
-        return user_query
+
+        if not user_query:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"The user with ID {id_user} was not found."
+                )
         
+        if not validate_create(user):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="The provided user data is invalid. Please check the input fields."
+            )
+
+        user_query.name = user.name
+        user_query.login = user.login
+        user_query.number = user.number
+        user_query.password = user.password
+        self.session.commit()
+        return None
+    
