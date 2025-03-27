4cd50dd81e7c42ecef605694a6193ca558dd6ba8
Stevenn <stevenndev8@gmail.com>
2025-03-27 01:59:32 -0300

    refactor code, add error handling

--- src/infra/sqlalchemy/repository/product_repository.py
+++ src/infra/sqlalchemy/repository/product_repository.py
@@ -1,6 +1,9 @@
 from sqlalchemy.orm import Session
-from src.schema.schemas import ProductBase, ProductCreate, ProductEdit, ProductResponse
+from fastapi import HTTPException, status
+from src.schema.schemas import ProductBase, ProductCreate, ProductEdit, ProductResponse, ProductList, ProductAll
 from src.infra.sqlalchemy.models import models
+from src.infra.validators.product_validators import *
+from src.infra.validators.product_validators import create_product as update_product
 
 
 class ProcessProduct:
@@ -8,6 +11,20 @@ class ProcessProduct:
         self.session = session
     
     def create(self, product: ProductCreate):
+        if not create_product(product):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="The provided user data is invalid. Please check the input fields."
+                )
+        
+        user_query = self.session.query(models.User.id).filter(models.User.id == product.user_id).first()
+
+        if not user_query:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"The user with ID {product.user_id} was not found."
+                )
+
         db_product = models.Product(
                                     name=product.name,
                                     user_id=product.user_id,
@@ -16,56 +33,68 @@ class ProcessProduct:
                                     quantity=product.quantity,
                                     )
         
-        if db_product.quantity > 0:
+        if avaliable_product(db_product.quantity):
             db_product.avaliable=True
 
         self.session.add(db_product)
         self.session.commit()
         self.session.refresh(db_product)
-        return db_product
+        return ProductResponse.model_validate(db_product)
      
     def to_list(self):
         products = self.session.query(models.Product).all()
-        return products 
+        if not products:
+            raise HTTPException(
+                status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+                detail="Service is unavailable. Fix in progress."
+                )
+
+        return [ProductList.model_validate(product) for product in products]
 
     def search(self, id_query: int):
         result = self.session.query(models.Product).filter(models.Product.id == id_query).first()
-        return ProductResponse(
-                            id=result.id,
-                            user_id=result.user_id,
-                            name=result.name,
-                            description=result.description,
-                            price=result.price,
-                            quantity=result.quantity,
-                            avaliable=result.avaliable
-                            
-        )
+        if not result:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"The product with ID {id_query} was not found."
+                )
+
+        return ProductAll.model_validate(result)
 
     def delete_product(self, id_prd: int):
         query = self.session.query(models.Product).filter(models.Product.id == id_prd).first()
-        if query:
-            self.session.delete(query)
-            self.session.commit()
-            return f'The product {query.name} has been deleted'
-        return False
+        if not query:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"The product with ID {id_query} was not found."
+                )
+
+        self.session.delete(query)
+        self.session.commit()
+        return None
     
     def edit_product(self, id_user: int, product: ProductEdit):
+        if not update_product(product):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"The product with ID {id_user} was not found."
+            )
+
         query = self.session.query(models.Product).filter(models.Product.id == id_user).first()
-        if query:
-            query.name = product.name
-            query.description = product.description
-            query.price = product.price
-            query.quantity = product.quantity
-            if product.quantity > 0:
-                query.avaliable = True
-            self.session.commit()
-            self.session.refresh(query)
-            return ProductResponse(
-                            id=result.id,
-                            user_id=result.user_id,
-                            name=result.name,
-                            description=result.description,
-                            price=result.price,
-                            quantity=result.quantity,
-                            avaliable=result.avaliable        
-        )
+        
+        if not query:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"The product with ID {id_user} was not found."
+            )
+
+        query.name = product.name
+        query.description = product.description
+        query.price = product.price
+        query.quantity = product.quantity
+        if quantity_product(query.quantity):
+            query.avaliable = True
+
+        self.session.commit()
+        self.session.refresh(query)
+        return None
--- src/routers/product_router.py
+++ src/routers/product_router.py
@@ -1,48 +1,39 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
+from typing import List
 from src.infra.sqlalchemy.repository.product_repository import ProcessProduct
-from src.schema.schemas import ProductBase, ProductCreate, ProductEdit, ProductResponse
+from src.schema.schemas import ProductBase, ProductCreate, ProductEdit, ProductResponse, ProductList, ProductAll
 from src.infra.sqlalchemy.config.database import get_db
-from typing import List, Union
 
 
 router = APIRouter()
 
 
-@router.post('/product', status_code=status.HTTP_201_CREATED, response_model=ProductCreate)
+@router.post('/product', status_code=status.HTTP_201_CREATED, response_model=ProductResponse)
 def add_product(product: ProductCreate, session: Session = Depends(get_db)):
     prd_db = ProcessProduct(session).create(product)
-    if not prd_db:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="The Body has an incorrect format.")
     return prd_db
 
 
-@router.get('/product', status_code=status.HTTP_200_OK)
+@router.get('/product', status_code=status.HTTP_200_OK, response_model=List[ProductList])
 def to_list_prds(session: Session = Depends(get_db)):
     list_prd = ProcessProduct(session).to_list()
-    if not list_prd:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No products registered.")
     return list_prd
 
 
-@router.get('/product/{id_query}', status_code=status.HTTP_200_OK, response_model=ProductResponse)
+@router.get('/product/{id_query}', status_code=status.HTTP_200_OK, response_model=ProductAll)
 def query_product(id_query: int, session: Session = Depends(get_db)):
     result = ProcessProduct(session).search(id_query)
-    if not result:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The product with ID {id_query} was not found.")
     return result
 
-@router.put('/product/{id_prod}', status_code=status.HTTP_200_OK, response_model=ProductResponse)
+
+@router.put('/product/{id_prod}', status_code=status.HTTP_204_NO_CONTENT)
 def edit_product(id_prod: int, product: ProductEdit, session: Session = Depends(get_db)):
     result = ProcessProduct(session).edit_product(id_prod, product)
-    if not result:
-        raise HTTPException(status_code=400, detail="The Body has an incorrect format.")
     return result
 
 
 @router.delete('/product/{id_prd}', status_code=status.HTTP_204_NO_CONTENT)
 def del_product(id_prd: int, session: Session = Depends(get_db)):
     result = ProcessProduct(session).delete_product(id_prd)
-    if not result:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The product with ID {id_prd} was not found.")
     return result
