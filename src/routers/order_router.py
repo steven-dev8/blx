002baaf806ec1c39aed9556c228cac3c6f9f7afb
Stevenn <stevenndev8@gmail.com>
2025-03-30 02:39:41 -0300

    add an endpoint to list order

--- src/routers/order_router.py
+++ src/routers/order_router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from typing import List
 from src.infra.sqlalchemy.repository.order_repository import ProcessOrder
-from src.schema.schemas import OrderCreate
+from src.schema.schemas import OrderCreate, OrderResponse
 from src.infra.sqlalchemy.config.database import get_db
 
 
@@ -12,4 +12,10 @@ router = APIRouter()
 @router.post('/order', status_code=status.HTTP_201_CREATED)
 def create_order(order: OrderCreate, session: Session = Depends(get_db)):
     result = ProcessOrder(session).create_order(order)
+    return result
+
+
+@router.get('/order', status_code=status.HTTP_200_OK, response_model=List[OrderResponse])
+def list_order(session: Session = Depends(get_db)):
+    result = ProcessOrder(session).list_order()
     return result
