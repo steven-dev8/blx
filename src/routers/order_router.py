0283da800b20a34569582376b8558a0e3b4fda13
stevenn-dev8 <stevenndev8@gmail.com>
2025-04-10 02:19:59 -0300

    WIP(auth): implement JWT authentication into order method

--- src/routers/order_router.py
+++ src/routers/order_router.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from typing import List
 from src.infra.sqlalchemy.repository.order_repository import ProcessOrder
-from src.schema.schemas import OrderCreate, OrderResponse, OrderResponseSearch
+from src.schema.schemas import OrderCreate, OrderResponse, OrderResponseSearch, UserLoginOut
+from src.routers.auth_utils import get_registered_user
 from src.infra.sqlalchemy.config.database import get_db
 
 
@@ -10,8 +11,11 @@ router = APIRouter()
 
 
 @router.post('/order', status_code=status.HTTP_201_CREATED)
-def create_order(order: OrderCreate, session: Session = Depends(get_db)):
-    result = ProcessOrder(session).create_order(order)
+def create_order(order: OrderCreate,
+                 user: UserLoginOut = Depends(get_registered_user),
+                 session: Session = Depends(get_db)):
+    print('foi')
+    result = ProcessOrder(session).create_order(order, user.id)
     return result
 
 
