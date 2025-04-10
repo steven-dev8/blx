1e1af3f2cc18a5c00df4b2436275380f3a4134a0
stevenn-dev8 <stevenndev8@gmail.com>
2025-04-10 02:16:36 -0300

    feat(auth): implement JWT authentication into user methods

--- src/routers/user_router.py
+++ src/routers/user_router.py
@@ -1,45 +1,44 @@
-from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
+from typing import List
 from src.infra.sqlalchemy.repository.user_repository import ProcessUser
-from src.schema.schemas import UserProduct, UserResponse, UserBase, UserCreate, UserEdit
+from src.schema.schemas import UserProduct, UserResponse, UserEdit, UserLoginOut
 from src.infra.sqlalchemy.config.database import get_db
-from typing import List, Union
+from src.routers.auth_utils import get_registered_user
 
 
 router = APIRouter()
 
 
-@router.get('/user', status_code=status.HTTP_200_OK, response_model=List[UserResponse])
+@router.get('/user/list', status_code=status.HTTP_200_OK, response_model=List[UserResponse])
 def to_list_users(session: Session = Depends(get_db)):
     list_user = ProcessUser(session).to_list()
     return list_user
 
 
-@router.get('/user/{id_user}', status_code=status.HTTP_200_OK, response_model=UserResponse)
-def query_user(id_user: int, session: Session = Depends(get_db)):
-    result_user = ProcessUser(session).query_user(id_user)
-    return result_user
+@router.delete('/user', status_code=status.HTTP_204_NO_CONTENT)
+def del_user(user: UserLoginOut = Depends(get_registered_user),
+             session: Session = Depends(get_db)):
+    ProcessUser(session).delete_user(user.id)
 
 
-@router.delete('/user/{id_user}', status_code=status.HTTP_204_NO_CONTENT)
-def del_user(id_user: int, session: Session = Depends(get_db)):
-    result = ProcessUser(session).delete_user(id_user)
-    return None
-
-
-@router.get('/user/{id_user}/products', status_code=status.HTTP_200_OK, response_model=UserProduct)
-def search_user_product(id_user: int, session: Session = Depends(get_db)):
-    result = ProcessUser(session).user_product(id_user) 
+@router.get('/user/products', status_code=status.HTTP_200_OK, response_model=UserProduct)
+def search_user_product(user: UserLoginOut = Depends(get_registered_user),
+                        session: Session = Depends(get_db)):
+    result = ProcessUser(session).user_product(user.id) 
     return result
 
 
-@router.put('/user/{id_user}', status_code=status.HTTP_204_NO_CONTENT)
-def edit_user(id_user: int, user: UserEdit, session: Session = Depends(get_db)):
-    result = ProcessUser(session).edit_user(id_user, user)
+@router.put('/user', status_code=status.HTTP_204_NO_CONTENT)
+def edit_user(user_edit: UserEdit,
+              user: UserLoginOut = Depends(get_registered_user),
+              session: Session = Depends(get_db)):
+    result = ProcessUser(session).edit_user(user.id, user_edit)
     return result
 
 
-@router.get('/user/{id_query}/orders')
-def list_orders(id_query: int, session: Session = Depends(get_db)):
-    result = ProcessUser(session).user_order(id_query)
+@router.get('/user/orders')
+def list_orders(user: UserLoginOut = Depends(get_registered_user),
+                session: Session = Depends(get_db)):
+    result = ProcessUser(session).user_order(user.id)
     return result
