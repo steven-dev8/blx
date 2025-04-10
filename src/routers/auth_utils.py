7030952abcaa41e8b1d398622320e1d52bdff98a
stevenn-dev8 <stevenndev8@gmail.com>
2025-04-10 01:55:46 -0300

    feat(auth): add JWT authentication

---
+++ src/routers/auth_utils.py
@@ -0,0 +1,32 @@
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordBearer
+from src.infra.sqlalchemy.config.database import get_db
+from src.infra.providers import token_provider
+from src.infra.sqlalchemy.repository.user_repository import ProcessUser
+from jwt import PyJWTError
+
+oauth2_schema = OAuth2PasswordBearer(tokenUrl='token')
+
+def get_registered_user(token: str = Depends(oauth2_schema),
+                        session: Session = Depends(get_db)):
+    
+    exception = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token"
+            )
+    
+    try:
+        login = token_provider.validate_access_token(token)
+    except PyJWTError:
+        raise exception
+    
+    if not login:
+        raise exception
+    
+    user = ProcessUser(session).search_user_login(login)
+
+    if not user:
+        raise exception
+
+    return user
