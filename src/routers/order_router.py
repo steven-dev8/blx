010adde1e1003160b20416ea5fab886ffe67b585
stevenn-dev8 <stevenndev8@gmail.com>
2025-04-12 22:19:13 -0300

    feat: add search_vendor_order and search_client_order methods; rename all method names

--- src/routers/order_router.py
+++ src/routers/order_router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from typing import List
 from src.infra.sqlalchemy.repository.order_repository import ProcessOrder
-from src.schema.schemas import OrderCreate, OrderResponse, OrderResponseSearch, UserLoginOut
+from src.schema.schemas import OrderCreate, OrderResponse, OrderResponse, UserLoginOut
 from src.routers.auth_utils import get_registered_user
 from src.infra.sqlalchemy.config.database import get_db
 
@@ -11,33 +11,48 @@ router = APIRouter()
 
 
 @router.post('/order', status_code=status.HTTP_201_CREATED)
-def create_order(order: OrderCreate,
+def create_client_order(order: OrderCreate,
                  user: UserLoginOut = Depends(get_registered_user),
                  session: Session = Depends(get_db)):
-    print('foi')
     result = ProcessOrder(session).create_order(order, user.id)
     return result
 
 
-@router.get('/order', status_code=status.HTTP_200_OK, response_model=List[OrderResponse])
-def list_order(session: Session = Depends(get_db)):
-    result = ProcessOrder(session).list_order()
+@router.get('/order/client', status_code=status.HTTP_200_OK, response_model=List[OrderResponse])
+def list_client_orders(user: UserLoginOut = Depends(get_registered_user),
+               session: Session = Depends(get_db)):
+    result = ProcessOrder(session).list_user_order(user.id)
     return result
 
 
-@router.get('/order/{id_order}', status_code=status.HTTP_200_OK, response_model=OrderResponseSearch)
-def search_order(id_order: int, session: Session = Depends(get_db)):
-    result = ProcessOrder(session).search_order(id_order)
+@router.get('/order/client/{id_order}', status_code=status.HTTP_200_OK, response_model=OrderResponse)
+def search_client_order(id_order: int,
+                 user: UserLoginOut = Depends(get_registered_user),
+                 session: Session = Depends(get_db)):
+    result = ProcessOrder(session).search_order(id_order, user.id)
     return result
 
 
-@router.delete('/order/{id_order}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_order(id_order: int, session: Session = Depends(get_db)):
-    result = ProcessOrder(session).delete_order(id_order)
-    return result
+@router.delete('/order/client/{id_order}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_client_order(id_order: int,
+                 user: UserLoginOut = Depends(get_registered_user),
+                 session: Session = Depends(get_db)):
+    ProcessOrder(session).delete_order(id_order, user.id)
+
+
+@router.get('/order/vendor/{id_order}', status_code=status.HTTP_200_OK, response_model=OrderResponse)
+def search_vendor_orders(id_order: int,
+                         user: UserLoginOut = Depends(get_registered_user),
+                         session: Session = Depends(get_db)):
+    order = ProcessOrder(session).search_vendor_order(id_order, user.id)
+    return order
+
 
 
 @router.patch('/order/{id_order}', status_code=status.HTTP_204_NO_CONTENT)
-def change_status(id_order: int, status: bool = False, session: Session = Depends(get_db)):
-    result = ProcessOrder(session).change_status(id_order, status)
+def change_status(id_order: int,
+                  status: bool = False,
+                  user: UserLoginOut = Depends(get_registered_user),
+                  session: Session = Depends(get_db)):
+    result = ProcessOrder(session).change_status(id_order, user.id, status)
     return result
